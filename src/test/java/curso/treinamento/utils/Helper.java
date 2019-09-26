2a44377b19d12b435734495aaaa914fb965432a4
Douglas Andrade <sousaandradedouglas@gmail.com>
2019-09-26 00:38:58 -0300

    projeto inicializado

---
+++ src/test/java/curso/treinamento/utils/Helper.java
@@ -0,0 +1,24 @@
+package curso.treinamento.utils;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import curso.treinamento.setup.Hooks;
+
+public class Helper {
+	
+	
+	public static void aguardar_elemento(WebElement elemento, int timeout) { 
+		
+		WebDriverWait aguardar = new WebDriverWait(Hooks.getDriver(), timeout);
+		aguardar.until(ExpectedConditions.visibilityOf(elemento));		
+	}
+	
+	public static boolean elemento_existe(WebElement elemento, int timeout) {
+		aguardar_elemento(elemento,timeout);
+		
+		return elemento.isDisplayed();
+	}
+
+}
